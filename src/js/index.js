d11b6d622f8c1e1bdfda6e7882ad8f6b4bd572c3
AndriiMishch <anderito02@gmail.com>
2024-05-02 00:38:34 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 import Notiflix from "notiflix";
 
+const form = document.querySelector('form')
 const addHorseBtn = document.querySelector('[data-addHorse]')
 
-addHorseBtn.addEventListener('click', ()=>{
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(e) {
+    e.preventDefault();
     Notiflix.Notify.success('Horse succesfuly added')
-})
+    createMarkup()
+}
+function createMarkup() {
+    
+}
